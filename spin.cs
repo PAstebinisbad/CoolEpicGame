28a9c270aeae65fbb8505d5c48ae4bba59c327f3
e <160017499+Scratch-is-better@users.noreply.github.com>
2024-10-30 10:06:28 +1100

    dab on the h8's + respect 😂👌💯
    best code ever literally nothing better

--- spin.cs
+++ spin.cs
@@ -1,24 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class spin : MonoBehaviour
-{ public float a;
-  
-   public Transform dude;
-    // Start is called before the first frame update
+{
+    private float a;
+    private float b;
+    public Transform dude;
+    public bool up = false;
+    public bool down = true;
+    
+    public Vector3 pos;
+    public float floatSpeed = 1.0f;   // Controls the speed of the floating motion
+
+    public float floatDistance = 0.5f;
     void Start()
     {
         Transform dude = GameObject.Find("DefaultMale").transform;
-        a = 1;
+        a = 0;
+        b = 0;
 
+        pos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        dude.transform.Rotate(0,a, 0);
-        Debug.Log(transform.rotation.x);
-        a += .0001f;
+        dude.transform.Rotate(0, .7f, 0);
+      
+        a += .005f;
+
+        b = .5f * Mathf.Sin(a)+1;
+        transform.position = new Vector3(pos.x, b, pos.z);
     }
 }
+
